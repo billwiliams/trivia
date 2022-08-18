554c3ea294d60dd70c601a1d7d404afdd3b5a43c
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-08-18 13:24:16 +0300

    Add Error 405 "
    Define not allowed error

--- backend/error_routes.py
+++ backend/error_routes.py
@@ -6,8 +6,9 @@ errors = Blueprint('errors', __name__)
 @errors.app_errorhandler(404)
 def not_found(error):
     return (
-        jsonify({"success": False, "error": 404,
-                "message": "resource not found"}),
+        jsonify({"success": False,
+                "error": 404,
+                 "message": "resource not found"}),
         404,
     )
 
@@ -15,12 +16,28 @@ def not_found(error):
 @errors.app_errorhandler(422)
 def unprocessable(error):
     return (
-        jsonify({"success": False, "error": 422,
-                "message": "unprocessable"}),
+        jsonify({"success": False,
+                "error": 422,
+                 "message": "unprocessable"}),
         422,
     )
 
 
 @errors.app_errorhandler(400)
 def bad_request(error):
-    return jsonify({"success": False, "error": 400, "message": "bad request"}), 400
+    return (jsonify({"success": False,
+                    "error": 400,
+                     "message": "bad request"}),
+            400,
+            )
+
+
+@errors.app_errorhandler(405)
+def not_allowed(error):
+    return (
+        jsonify({"success": False,
+                 "error": 400,
+                 "message": "not allowed"}),
+        405,
+
+    )
