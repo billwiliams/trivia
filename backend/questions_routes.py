989db6d4f7e3849e380731f236fedad4b00ee773
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-08-19 10:56:05 +0300

    Add All Categories to question query

--- backend/questions_routes.py
+++ backend/questions_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, abort, request
-from models import Question
+from models import Question, Category
 from utils import paginate
 
 
@@ -9,8 +9,11 @@ QUESTIONS_PER_PAGE = 10
 
 @questions.route("/questions")
 def retrieve_questions():
-    questions_selection = Question.query.order_by(Question.id).all()
+    questions_selection = Question.query.order_by(
+        Question.id).join(Category, Category.id == Question.category).all()
+    print(questions_selection[0])
     questions = paginate(request, questions_selection, QUESTIONS_PER_PAGE)
+    categories = [item.type for item in Category.query.all()]
     if len(questions) == 0:
         abort(404)
 
@@ -18,6 +21,8 @@ def retrieve_questions():
         {
             "success": True,
             "questions": questions,
-            "total_questions": len(questions),
+            "total_questions": len(questions_selection),
+            "categories": categories,
+            "current_category": 0,
         }
     )
